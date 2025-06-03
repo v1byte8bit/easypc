3baf425b365db22cacac0a44b6bb3c43cf40d937
v1byte8bit <il.russckix@yandex.ru>
2025-06-03 13:44:57 +0500

    update

--- src/main/java/com/example/easypc/parse/Bit28ProductParser.java
+++ src/main/java/com/example/easypc/parse/Bit28ProductParser.java
@@ -12,8 +12,6 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Component
 public class Bit28ProductParser implements ProductParser {
